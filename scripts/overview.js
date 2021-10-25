a6601f4894b68eb9ff9e9aa13ad184d31f825916
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2021-10-25 23:13:08 +0100

    Base for edit and remove buttons

--- scripts/overview.js
+++ scripts/overview.js
@@ -101,7 +101,11 @@ function displayOverviewData() {
   data.supplements.forEach(element => {
     document.querySelector(".overview-items").innerHTML +=
     `<div class="overview-row">
-      <span class="overview-nutrient overview-column">${element.name}</span>
+      <div class="overview-editor overview-nutrient overview-column">
+          <span class="overview-icon overview-remove">&#9003;</span>
+          <span class="overview-icon overview-edit">&#9998;</span>
+        <span>${element.name}</span>
+      </div>
       <span class="overview-dosage overview-column">${element.product.amount + " " + element.nutrient.unit}</span>
       <span class="overview-serving overview-column">${element.personal.servings}</span>
       <span class="overview-time overview-column">${element.personal.time}</span>
