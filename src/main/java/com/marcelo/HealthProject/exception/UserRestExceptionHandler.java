fcbe3bdf725744a8abeed268f22e72702365ccd5
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-09-28 21:31:32 +0100

    Created Exception Handler

---
+++ src/main/java/com/marcelo/HealthProject/exception/UserRestExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.marcelo.HealthProject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UserRestExceptionHandler {
+	
+	
+	// handler for user not found exception
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse> handleException(UserNotFoundException exc) {
+		
+		UserErrorResponse error = new UserErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	// generic exception handler to catch any exception (catch all)
+	
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse> handleException(Exception exc) {
+		
+		UserErrorResponse error = new UserErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+				
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+}
