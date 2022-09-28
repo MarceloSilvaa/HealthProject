7f8ea39d0962ee156256446a756e5f1d8e6e4641
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-09-28 21:32:21 +0100

    Added more endpoints to the user rest controller

--- src/main/java/com/marcelo/HealthProject/rest/UserRestController.java
+++ src/main/java/com/marcelo/HealthProject/rest/UserRestController.java
@@ -3,11 +3,17 @@ package com.marcelo.HealthProject.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcelo.HealthProject.entity.User;
+import com.marcelo.HealthProject.exception.UserNotFoundException;
 import com.marcelo.HealthProject.service.UserService;
 
 @RestController
@@ -17,8 +23,48 @@ public class UserRestController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/all")
+	@GetMapping("")
 	public List<User> findAll() {
 		return userService.findAll();
+	}
+	
+	@GetMapping("/{userId}")
+	public User findById(@PathVariable int userId) {
+		User user = userService.findById(userId);
+		
+		if(user == null) {
+			throw new UserNotFoundException("Could not find user with id " + userId);
+		}
+		
+		return user;
+	}
+	
+	@PostMapping("")
+	public User add(@RequestBody User user) {
+		user.setId(0);
+		
+		userService.save(user);
+		
+		return user;
+	}
+	
+	@PutMapping("")
+	public User update(@RequestBody User user) {
+		userService.save(user);
+		
+		return user;
+	}
+	
+	@DeleteMapping("/{userId}")
+	public String deleteById(@PathVariable int userId) {
+		User user = userService.findById(userId);
+		
+		if(user == null) {
+			throw new UserNotFoundException("Could not find user with id " + userId);
+		}
+		
+		userService.deleteById(userId);
+		
+		return "Deleted user with id " + userId;
 	}
 }
