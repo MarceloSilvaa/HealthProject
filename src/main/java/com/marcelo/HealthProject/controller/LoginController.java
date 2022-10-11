d51357927ba36b5a6326b57636faf84aedf4432f
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-10-11 17:38:43 +0100

    Forbidden page

--- src/main/java/com/marcelo/HealthProject/controller/LoginController.java
+++ src/main/java/com/marcelo/HealthProject/controller/LoginController.java
@@ -11,8 +11,8 @@ public class LoginController {
 		return "login";
 	}
 	
-	@GetMapping("/accessDenied")
+	@GetMapping("/forbidden")
 	public String showAccessDenied() {
-		return "access-denied";
+		return "forbidden";
 	}
 }
