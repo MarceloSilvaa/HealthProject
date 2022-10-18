dbb15638d6f2825ad212bdecadc0ffbabd8ca181
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-10-18 18:32:19 +0100

    Supplements Controller

---
+++ src/main/java/com/marcelo/HealthProject/controller/SupplementsController.java
@@ -0,0 +1,31 @@
+package com.marcelo.HealthProject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.HealthProject.entity.Supplement;
+import com.marcelo.HealthProject.service.SupplementService;
+
+@RestController
+@RequestMapping("/supplements")
+public class SupplementsController {
+	
+	@Autowired
+	private SupplementService supplementService;
+	
+	@GetMapping("/{userId}")
+	public List<Supplement> findAllByUserId(@PathVariable int userId) {
+		return supplementService.findAllByUserId(userId);
+	}
+	
+	@GetMapping("")
+	public List<Supplement> findAllByUsername(@RequestParam("username") String username) {
+		return supplementService.findAllByUsername(username);
+	}
+}
