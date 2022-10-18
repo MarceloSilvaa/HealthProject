6eb9c6da3e2ffa41fc1ad6c810c3b6e1d2ca3f51
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-10-18 19:04:14 +0100

    Supplement Controller

--- src/main/java/com/marcelo/HealthProject/controller/SupplementController.java
+++ src/main/java/com/marcelo/HealthProject/controller/SupplementController.java
@@ -14,7 +14,7 @@ import com.marcelo.HealthProject.service.SupplementService;
 
 @RestController
 @RequestMapping("/supplements")
-public class SupplementsController {
+public class SupplementController {
 	
 	@Autowired
 	private SupplementService supplementService;
