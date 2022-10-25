62dbf66c55dd9298694ace4e7a2d8bbc7c46a159
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-10-25 13:36:52 +0100

    Created exception handler for supplement not found error

--- src/main/java/com/marcelo/HealthProject/exception/UserRestExceptionHandler.java
+++ src/main/java/com/marcelo/HealthProject/exception/UserRestExceptionHandler.java
@@ -8,10 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class UserRestExceptionHandler {
 	
-	
 	// handler for user not found exception
 	@ExceptionHandler
 	public ResponseEntity<UserErrorResponse> handleException(UserNotFoundException exc) {
+		
+		UserErrorResponse error = new UserErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError(HttpStatus.valueOf(error.getStatus()).getReasonPhrase());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	// handler for supplement not found exception
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse> handleException(SupplementNotFoundException exc) {
 		
 		UserErrorResponse error = new UserErrorResponse();
 		
