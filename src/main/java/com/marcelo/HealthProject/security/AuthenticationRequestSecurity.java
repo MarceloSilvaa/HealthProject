67591ce1f110a16ae1b1ddf1ca59acc79230160d
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-10-31 19:04:58 +0000

    Match user id against authenticated user

--- src/main/java/com/marcelo/HealthProject/security/AuthenticationRequestSecurity.java
+++ src/main/java/com/marcelo/HealthProject/security/AuthenticationRequestSecurity.java
@@ -1,11 +1,17 @@
 package com.marcelo.HealthProject.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.marcelo.HealthProject.entity.User;
 import com.marcelo.HealthProject.exception.UserForbiddenException;
+import com.marcelo.HealthProject.service.UserService;
 
 public class AuthenticationRequestSecurity {
+	
+	@Autowired
+	private UserService userService;
 	
 	public String getAuthenticationUsername() {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -21,10 +27,23 @@ public class AuthenticationRequestSecurity {
 		return username;
 	}
 	
-	public void verifyUsername(String username) {
+	public Integer getAuthenticationUserId() {
+		User user = userService.findByUsernameCustomVerify(getAuthenticationUsername());
+		return user.getId();
+	}
+	
+	public boolean isAuthenticatedUser(String username) {
 		if(!username.equals(getAuthenticationUsername())) {
 			throw new UserForbiddenException("User is not allowed to make this request");
 		}
+		return true;
+	}
+	
+	public boolean isAuthenticatedUser(Integer id) {
+		if(id != getAuthenticationUserId()) {
+			throw new UserForbiddenException("User is not allowed to make this request");
+		}
+		return true;
 	}
 	
 }
