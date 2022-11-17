192b515c851c6efa8b1c188cc3b13777ea4bc90e
MarceloSilvaa <marcelomsilvaaa@gmail.com>
2022-11-17 15:28:37 +0000

    Code for get request

--- src/main/resources/static/scripts/request.mjs
+++ src/main/resources/static/scripts/request.mjs
@@ -1,8 +1,23 @@
+export function getAsync(url) {
+	return new Promise(async function (resolve, reject) {
+		let response = await fetch(url, {
+			method: "GET",
+			headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    	},
+			credentials:"include"}
+		)
+		let data = await response.json()
+		resolve(data)
+	})
+}
+
 export async function deleteAsync(url) {
 	let response = await fetch(url)
 	if(response.status >= 200 && response.status < 300) {
 		if(response.redirected === true) {
-			window.location.replace(response.url);
+			window.location.replace(response.url)
 		}
 	}
 }
